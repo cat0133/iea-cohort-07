cbb556e93d57315248b754e6834bb37295726e4c
lynn <lynn.cates@libertymutual.com>
2022-05-10 19:16:59 -0400

    labs

---
+++ 05_python_for_devops/calmodule.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import IDLE
+
+def calculate(op_a, op_b,operator):
+    if operator == '+':
+        return op_a + op_b
+    if operator == '-':
+        return op_a - op_b
+    if operator == '*':
+        return op_a * op_b
+    if operator == '/':
+        try:
+            return op_a / op_b
+        except ZeroDivisionError:
+            print("Cannot divide by zero!")
+            return None
+        if operator == "log":
+            try:
+                return math.log(op_a) / math.log(op_b)
+            except ValuseError:
+                print('Cannot take log of 0!")
+                return None
+            except ZeroDivisionError:
+                print("Cannot take log base 1!")
+                return None
+            else:
+                return result
